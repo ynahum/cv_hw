29aae62369d51defc0600d59fb3eed79ba2ccc7b
Yair Nahum <yair.y.nahum@intel.com>
2021-07-08 22:57:56 +0300

    HW4 small fixes to word and reformating for Questions sections + not tested code for SIFT

--- Hw4/code/my_homography.py
+++ Hw4/code/my_homography.py
@@ -1,4 +1,5 @@
 # %% Imports
+from builtins import range
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,28 +12,41 @@ from skimage import color
 
 # Our functions
 
-def plotCleanImages(im1,im2,title1,title2):
+def plotImage(im,title):
+    plt.figure()
+    plt.axis('off')
+    plt.imshow(im)
+    plt.title(title)
+
+
+def plotTwoImages(im1,im2,title1,title2):
     plt.figure()
     plt.subplot(1,2,1)
+    plt.axis('off')
     plt.imshow(im1)
     plt.title(title1)
     plt.subplot(1,2,2)
+    plt.axis('off')
     plt.imshow(im2)
     plt.title(title2)
+
 
 def d2ToHomoginic(p):
     pHom = np.concatenate((p,np.ones((1,p.shape[1]))))
     return pHom
 
+
 def getForwardTransformation(p,H2to1,epsilon = 1e-14):
     pTransformed = H2to1@p
-    pTransformed = pTransformed / (pTransformed[2,:]+epsilon) # to avoide zeros
+    pTransformed = pTransformed / (pTransformed[2,:]+epsilon) # to avoid zeros
     return pTransformed
 
-def getInvTransformation(p,H2to1):
+
+def getInvTransformation(p,H2to1,epsilon = 1e-14):
     pTransformed = np.linalg.pinv(H2to1)@p
-    pTransformed = pTransformed / pTransformed[2,:]
+    pTransformed = pTransformed / (pTransformed[2,:]+epsilon)
     return pTransformed
+
 
 def evaluateHmatrix(p1,p2,H2to1):
     p1_ = d2ToHomoginic(p1)
@@ -43,19 +57,22 @@ def evaluateHmatrix(p1,p2,H2to1):
     p2temp = p2temp[:-1,:]
     return p2temp
 
-def PlotEstimatedTransformation(im1,im2,p1,p2,p2reconstructed):
+
+def PlotEstimatedTransformation(im1, im2, im1_title, im2_title, p1, p2, p2reconstructed):
     plt.figure()
     plt.subplot(1,2,1)
     plt.imshow(im1)
     plt.scatter(p1[0,:],p1[1,:],label='Actual location')
     plt.legend()
-    plt.title('incline_L')
+    plt.title(im1_title)
+    plt.axis('off')
     plt.subplot(1,2,2)
     plt.imshow(im2)
     plt.scatter(p2[0,:],p2[1,:],label='Actual location')
     plt.scatter(p2reconstructed[0,:],p2reconstructed[1,:],label='Estimated transformed location')
     plt.legend()
-    plt.title('incline_R')
+    plt.title(im2_title)
+    plt.axis('off')
 
 
 def GetCornersAfterTransform(im1,H):
@@ -69,6 +86,7 @@ def GetCornersAfterTransform(im1,H):
     RT = rightCorners[:, rightCorners[1].argmin()]; RB = rightCorners[:, rightCorners[1].argmax()]
     return LT, LB, RT, RB
 
+
 def ShiftH(im1, H):
     LT, LB, RT, RB = GetCornersAfterTransform(im1, H)
     Left = np.int64(np.min((LT[0], LB[0])))
@@ -80,6 +98,7 @@ def ShiftH(im1, H):
     tMatrix = np.array([[1, 0, Left],[0, 1, Top],[0, 0, 1]])
     HShifted = H @ tMatrix
     return HShifted, out_size,corners
+
 
 def ResizeImg(img1, wrap_img2, corners):
     # corners = (Left, Right, Top, Bottom)
@@ -93,12 +112,54 @@ def ResizeImg(img1, wrap_img2, corners):
     im1_full[im1_maskInd[0] - corners[2], im1_maskInd[1] - corners[0], im1_maskInd[2]] = img1[im1_maskInd]
     return np.uint8(im1_full), np.uint8(warp_im2_full)
 
+
+def Q1_1_get_points(im1, im2, N, im1_title, im2_title, plot_images=True, get_from_user=True):
+    if plot_images:
+        plotTwoImages(im1, im2, im1_title, im2_title)
+    return getPoints(im1, im2, N, im1_title, im2_title, get_from_user=get_from_user)
+
+
+def Q1_2_compute_H(im1, im2, im1_title, im2_title, p1, p2, plot_estimated_transform=True):
+    H2to1 = computeH(p1, p2)
+    p2reconstructed = evaluateHmatrix(p1, p2, H2to1)
+    if plot_estimated_transform:
+        PlotEstimatedTransformation(im1, im2, im1_title, im2_title, p1, p2, p2reconstructed)
+    H2to1Shifted,out_size,corners = ShiftH(im1, H2to1)
+    H = H2to1Shifted.copy()
+    return H,out_size,corners
+
+
+def Q1_3_warp(im1, H, out_size, plot_warp=True, run_both_interp_methods=False):
+    warp_im1_linear = warpH(im1, H, out_size)
+    warp_im1_linear_title = 'Linear interpolation'
+    if run_both_interp_methods:
+        warp_im1_cubic = warpH(im1, H, out_size,kind='cubic')
+        if plot_warp:
+            plotTwoImages(warp_im1_linear, warp_im1_cubic, warp_im1_linear_title, 'Cubic interpolation')
+    else:
+        if plot_warp:
+            plotImage(warp_im1_linear, warp_im1_linear_title)
+    return warp_im1_linear
+
+
+def Q1_4_stitch(img2, warped_img1, corners, plot_stitch=True):
+    im1_full, warp_im2_full = ResizeImg(img2, warped_img1, corners)
+    panoImg = imageStitching(im1_full, warp_im2_full)
+    if plot_stitch:
+        plotImage(panoImg,'Image  stitching')
+    return im1_full, warp_im2_full
+
+
+#---------------------------
+
 # HW functions:
-def getPoints(im1,im2,N,im1_title=None, im2_title=None, get_from_user=True):
+
+
+def getPoints(im1, im2, N, im1_title=None, im2_title=None, get_from_user=True):
 
     plt.figure()
     plt.subplot(1, 2, 1)
-
+    plt.axis('off')
     plt.imshow(im1)
     if im1_title != None:
         plt.title(im1_title)
@@ -129,6 +190,7 @@ def getPoints(im1,im2,N,im1_title=None, im2_title=None, get_from_user=True):
 
     return p1,p2
 
+
 def computeH(p1, p2):
     assert (p1.shape[1] == p2.shape[1])
     assert (p1.shape[0] == 2)
@@ -152,6 +214,7 @@ def computeH(p1, p2):
     H2to1 = np.reshape(vectors[:,-1],(3,3))
     return H2to1
 
+
 def warpH(im1, H, out_size,kind='linear'):
     im1_float = im1/255
     warp_im1 = np.zeros((out_size[0],out_size[1],3))
@@ -165,10 +228,10 @@ def warpH(im1, H, out_size,kind='linear'):
     x_out = x_out[inside]; y_out = y_out[inside]
     for channel in range(im1_float.shape[-1]):
         f = interp2d(np.arange(im1_float.shape[1]), np.arange(im1_float.shape[0]), im1_float[:, :, channel], kind=kind,fill_value=0)
-        intepolationArray = np.array([f(XX, YY)[0] for XX, YY in zip(x_in, y_in)])
-        warp_im1[y_out, x_out, channel] = intepolationArray
+        warp_im1[y_out, x_out, channel] = np.array([f(x_value, y_value)[0] for x_value, y_value in zip(x_in, y_in)])
     warp_im1 = (warp_im1 * 255).astype('uint8')
     return warp_im1
+
 
 def imageStitching(img1, wrap_img2):
     panoImg = np.zeros(img1.shape)
@@ -179,10 +242,34 @@ def imageStitching(img1, wrap_img2):
     panoImg[im2_wrap_mask] = wrap_img2[im2_wrap_mask]
     return np.uint8(panoImg)
 
+
 def ransacH(matches, locs1, locs2, nIter, tol):
     return bestH
 
+
 def getPoints_SIFT(im1,im2):
+
+    # Initiate SIFT detector
+    sift = cv2.SIFT_create()
+
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = sift.detectAndCompute(im1, None)
+    kp2, des2 = sift.detectAndCompute(im2, None)
+
+    # BFMatcher with default params
+    bf = cv2.BFMatcher()
+    matches = bf.knnMatch(des1, des2, k=2)
+
+    # Apply ratio test
+    #good = []
+    #for m, n in matches:
+    #    if m.distance < 0.75 * n.distance:
+    #        good.append([m])
+
+    # cv.drawMatchesKnn expects list of lists as matches.
+    #img3 = cv2.drawMatchesKnn(im1, kp1, im2, kp2, good, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    #plt.imshow(img3), plt.show()
+
     return p1,p2
 
 # %% Main
@@ -192,32 +279,21 @@ if __name__ == '__main__':
     im1 =  cv2.cvtColor(cv2.imread('data/incline_L.png'), cv2.COLOR_BGR2RGB)
     im2 = cv2.cvtColor(cv2.imread('data/incline_R.png'), cv2.COLOR_BGR2RGB)
 
+    im1_title = 'incline_L'
+    im2_title = 'incline_R'
+
     # Q1.1
-    plot_clean_image = False
-    if plot_clean_image:
-        plotCleanImages(im1,im2,title1='incline_L',title2='incline_R')
     N = 5
-    p1,p2 = getPoints(im1,im2,N=N,im1_title='incline_L',im2_title='incline_R',get_from_user=False)
+    p1, p2 = Q1_1_get_points(im1, im2, N, im1_title, im2_title, plot_images=True, get_from_user=False)
 
     # Q1.2
-    H2to1 = computeH(p1, p2)
-    p2reconstructed = evaluateHmatrix(p1,p2,H2to1)
-    PlotEstimatedTransformation(im1,im2,p1,p2,p2reconstructed)
-    H2to1Shifted,out_size,corners = ShiftH(im1, H2to1)
-    H = H2to1Shifted.copy()
+    H, out_size, corners = Q1_2_compute_H(im1, im2, im1_title, im2_title, p1, p2, plot_estimated_transform=True)
 
     # Q1.3
-    warp_im1_linear = warpH(im1, H2to1Shifted, out_size)
-    plot_both_interpolations = False
-    if plot_both_interpolations:
-        warp_im1_cubic = warpH(im1, H2to1Shifted, out_size,kind='cubic')
-        plotCleanImages(warp_im1_linear,warp_im1_cubic,title1='Linear interpolation',title2='Cubic interpolation')
+    warp_im1_linear = Q1_3_warp(im1, H, out_size, plot_warp=True, run_both_interp_methods=False)
 
     # Q1.4
-    im1_full, warp_im2_full = ResizeImg(im2, warp_im1_linear,corners)
-    panoImg = imageStitching(im1_full, warp_im2_full)
-    plt.figure()
-    plt.imshow(panoImg)
-    plt.title('Image  stitching')
-    
+    im1_full, warp_im2_full = Q1_4_stitch(im2, warp_im1_linear, corners)
+
+    # Q1.5
     
